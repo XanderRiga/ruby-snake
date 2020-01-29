c9266040ee03db90de0ef568dea9240b18856923
Xander <xanderriga@live.com>
2020-01-28 21:09:32 -0800

    Log request payload on move call

--- app/move.rb
+++ app/move.rb
@@ -3,8 +3,8 @@
 require_relative '../lib/transforms/request_to_objects'
 
 def move(board)
+  puts 'request hash: ' + board
   request = Transforms::RequestToObjects.new.call(board)
-
   request.board.print_matrix
 
   { move: :up }
