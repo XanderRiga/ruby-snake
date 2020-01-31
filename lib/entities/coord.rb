545b20f66d28c0b0fd0dc0d98aef14b150fedc2d
Xander <xanderriga@live.com>
2020-01-30 17:23:10 -0800

    Add up, down, left, right on coords
    Also kill all the nonsense from Rubocop. I don't care nearly enough
    about cyclomatic complexity on a project like this.

--- lib/entities/coord.rb
+++ lib/entities/coord.rb
@@ -6,5 +6,21 @@ module Entities
   class Coord < Base
     attribute :x, Types::Strict::Integer
     attribute :y, Types::Strict::Integer
+
+    def up
+      Coord.new(x: x, y: y - 1)
+    end
+
+    def down
+      Coord.new(x: x, y: y + 1)
+    end
+
+    def left
+      Coord.new(x: x - 1, y: y)
+    end
+
+    def right
+      Coord.new(x: x + 1, y: y)
+    end
   end
 end
