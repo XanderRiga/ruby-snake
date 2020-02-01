ae0c4633cdaa50a64831f7d4f198c9d9eff85ade
Xander <xanderriga@live.com>
2020-01-31 17:42:22 -0800

    Comment out logging
    This is super helpful for debugging, but I think it is causing the
    snakes to run suuuuper slow.

--- app/move.rb
+++ app/move.rb
@@ -5,9 +5,9 @@ require_relative '../lib/interactors/choose_direction'
 require_relative '../lib/entities/coord'
 
 def move(board)
-  puts JSON.pretty_generate(board)
+  # puts JSON.pretty_generate(board)
   request = Transforms::RequestToObjects.new.call(board)
-  request.print_matrix
+  # request.print_matrix
 
   direction = Interactors::ChooseDirection.new.call(request)
 
